d44e8783e93df79642bd1f1d655e9225dbc7a6e7
Alex-Bailon <abailon949@gmail.com>
2024-10-28 19:38:10 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="250" />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  .logo {
+    margin: 2rem auto;
+    display: block;
+  }
+</style>
